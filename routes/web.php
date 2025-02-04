dab93a3785aad90a64427b8fba589755b59996ba
xturus138 <radityaramadhan.138@gmail.com>
2025-02-04 11:31:58 +0700

    fix: Added AuthController and routes updated for login functionality !!

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/example', function () {
     return view('example');
@@ -21,13 +20,13 @@ Route::get('/new_discussion', function () {
     return view('create_new_discussion.new_discussion');
 });
 
-Route::get('/home', function () {
-    return view('home.home');
-});
 
-Route::get('/login', function () {
-    return view('auth.login.login');
-});
+
+// Route::get('/login', function () {
+//     return view('auth.login.login');
+// });
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/register', function () {
     return view('auth.register.resgister');
@@ -36,3 +35,14 @@ Route::get('/register', function () {
 Route::get('/forget', function () {
     return view('auth.forget.forget');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/home', function () {
+        return view('home.home');
+    });
+});
+
